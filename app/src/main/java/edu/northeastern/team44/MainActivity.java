5643b991e9a0d31f0e6d3159bf10ad88caf788c5
QianruiXia8 <xia.qian@northeastern.edu>
2022-11-03 13:33:33 -0700

    initialize Stick It To 'Em activity include button and empty activity(Qianrui Xia)

--- app/src/main/java/edu/northeastern/team44/MainActivity.java
+++ app/src/main/java/edu/northeastern/team44/MainActivity.java
@@ -10,16 +10,22 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button atYourService;
+    Button stickItToEm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         atYourService = findViewById(R.id.atYourServiceButton);
+        stickItToEm = findViewById(R.id.b2);
 
         atYourService.setOnClickListener(view -> {
             Intent atYourServiceIntent = new Intent(view.getContext(), AtYourService.class);
             view.getContext().startActivity(atYourServiceIntent);
+        });
+        stickItToEm.setOnClickListener(view -> {
+            Intent stickItToEmIntent = new Intent(view.getContext(), Stick_It_To_Em.class);
+            view.getContext().startActivity(stickItToEmIntent);
         });
     }
 
---
+++ app/src/main/java/edu/northeastern/team44/Stick_It_To_Em.java
@@ -0,0 +1,14 @@
+package edu.northeastern.team44;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Stick_It_To_Em extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stick_it_to_em);
+    }
+}
